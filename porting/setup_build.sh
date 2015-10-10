f900081fb01ebe58ab9f91f6da3c34dd4d031e9e
gaojianzhong <gjz999666@126.com>
2015-10-10 10:42:49 +0800

    Update setup_build.sh
    delete company name

--- porting/setup_build.sh
+++ porting/setup_build.sh
@@ -1,4 +1,4 @@
 export ARCH=arm
 
 #change this to your own path
-export CROSS_COMPILE=/home/tted/Desktop/71SV/porting/arm-poky-linux-gnueabi/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
+export CROSS_COMPILE=71SV/porting/arm-poky-linux-gnueabi/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
